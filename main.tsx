b74a1a666428956bb865c7f618327da7fb415d5d
Br-iON <26982008+KaliStudent@users.noreply.github.com>
2025-08-25 23:39:27 -0500

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -2,7 +2,7 @@ document.documentElement.setAttribute('data-theme', 'sapphire-ice');
 import './index.css';
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import KombaiWrapper from 'KombaiWrapper'
+import KombaiWrapper from './KombaiWrapper'
 import ErrorBoundary from '@kombai/react-error-boundary'
 import './public/themes.css';
 
